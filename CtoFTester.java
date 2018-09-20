a082cd83d1adde071b0cd2b44d46885fac4685c9
DarthChurros <tnarayan00@stuy.edu>
2018-09-19 20:14:05 -0400

    Added conversion functions

---
+++ CtoFTester.java
@@ -0,0 +1,13 @@
+public class CtoFTester {
+  public static void main (String [] args) {
+    //function calls here
+  }
+}
+
+public double celsiusToFahrenheit (double c) {
+  return (c * 9 / 5 + 32);
+}
+
+public double fahrenheitToCelsius (double f) {
+  return ((c - 32) * 5 / 9);
+}
